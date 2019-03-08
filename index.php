831244ef69bf763666596a1ea83a086e6ddcdafb
118425304 <118425304@BISB74VL.bis.ad.ucc.ie>
2019-03-08 11:10:17 +0000

    Third commit

--- index.php
+++ index.php
@@ -5,13 +5,96 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
+   <meta charset="utf-8">
+
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+   <style>
+        body {
+        background-image: url("Background.jpg");
+        }
+       
+     
+        * {
+        box-sizing: border-box;
+        }
+
+        body {
+             margin: 0;
+            }
+
+      
+        .header {
+        background-color: #ddd;
+       
+        padding: 20px;
+       
+        text-align: center;
+        }
+
+
+        .topnav {
+       
+            overflow: hidden;
+        
+        background-color: #333;
+}
+
+
+        .topnav a {
+        
+            float: left;
+        
+            display: block;
+         
+            color: #f2f2f2;
+         
+            text-align: center;
+        
+            padding: 14px 210px;
+        
+            text-decoration: none;
+            }
+
+
+        .topnav a:hover {
+        
+            background-color: #ddd;
+          
+         color: black;
+            }
+    
+    
+    </style>
+    
+    
+    
+    
     <head>
+        <title>Homepage</title>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
+    
     <body>
-        <?php
-        // put your code here
-        ?>
+          <div class="header">
+  
+              <h1>Homepage</h1>
+    </div>
+
+    <div class="topnav">
+         <a href="cv.html">CV</a>
+         <a href="tiltedpage_scroll_demo.html">Interests</a>
+         <a href="Consultancy.html">Consultancy</a>
+    </div>
+        
+        
+       
+    
+    
+    
+    
+    
+    
     </body>
 </html>
